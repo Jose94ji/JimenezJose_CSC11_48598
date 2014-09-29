febddb58bcd5eef68c4ed56083216f892d720457
jose94ji <jose94ji@gmail.com>
2014-09-29 14:21:27 -0700

    Signed-off-by: jose94ji <jose94ji@gmail.com>

---
+++ Homework/Subtract.asm
@@ -0,0 +1,19 @@
+.text
+.global main
+main:
+	
+	mov r0, #101    /* a=101*/
+	mov r1, #5      /* b=5 */
+	mov r2, r0
+	mov r3, #0
+	
+	cmp r2, r1
+	
+	blt
+	subtract:
+	add r2, #1
+	sub r2, r2, r1
+	cmp r2, r1
+	
+	bge subtract
+	
