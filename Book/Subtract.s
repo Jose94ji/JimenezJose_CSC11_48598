989a7a0f39eac4979b620afe81be9c2ca65d0a6b
jose94ji <jjimenez123@student.rcc.edu>
2014-10-01 11:47:13 -0700

    Signed-off-by: as2466967 <safaie.azadeh@gmail.com>

--- Book/Subtract.s
+++ Book/Subtract.s
@@ -2,19 +2,19 @@
 .global main
 main:
 	
-	mov r0, #101    /* a=101*/
+	mov r2, #101    /* a=101*/
 	mov r1, #5      /* b=5 */
 	
 	mov r3, #0
-	mov r2, r0
-	cmp r2, r1
+	mov r0, r2
+	cmp r0, r1
 	
 	blt end
 	
 	subtract:
-	add r2, #1
-	sub r2, r2, r1
-	cmp r2, r1
+	add r3, #1
+	sub r0, r2, r1
+	cmp r0, r1
 	
 	bge subtract
 	
