cce861cd4ca45f892aa365f14bd8bfa0410e9348
jose94ji <jjimenez123@student.rcc.edu>
2014-10-01 12:26:59 -0700

    Signed-off-by: as2466967 <safaie.azadeh@gmail.com>

--- Book/Subtract.s
+++ Book/Subtract.s
@@ -1,22 +1,16 @@
 .text
 .global main
 main:
-	
 	mov r2, #101    /* a=101*/
 	mov r1, #5      /* b=5 */
-	
 	mov r3, #0
 	mov r0, r2
 	cmp r0, r1
-	
 	blt end
-	
 	subtract:
 	add r3, #1
 	sub r0, r2, r1
 	cmp r0, r1
-	
 	bge subtract
-	
 	end:
 	bx lr
